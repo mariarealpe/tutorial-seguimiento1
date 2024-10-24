d3bf49f09e082c959ccb1ed736bbe19da1ec5b98
maria josé realpe <mjrealpe_416@unihumboldt.edu.co>
2024-10-24 11:53:57 -0500

    Add files via upload

---
+++ ciclo.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 10; i++) {
+    if (i === 9) {
+        break; // Sale del ciclo cuando i es 9
+    }
+    console.log(i);
+}
---
+++ operadores.js
@@ -0,0 +1,7 @@
+let num = 21; // Número
+let nombre = "Miguel"; // String
+let esMayor = false; // Boolean
+let x; // Undefined
+let nulo = null; // Null
+
+console.log(num, nombre, esMayor, x, nulo);
